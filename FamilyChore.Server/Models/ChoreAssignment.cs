b55bbcb42da78f60fe755f4fc590491d5f4b7077
Carla Tillman <Carla.Tillman@prepass.com>
2025-02-26 16:14:32 -0700

    Chore Assignment model updated,.  Data added to  some of our 'source data'

---
+++ FamilyChore.Server/Models/ChoreAssignment.cs
@@ -0,0 +1,14 @@
+﻿using FamilyChore.Server.Data;
+using System.Linq.Expressions;
+
+namespace FamilyChore.Server.Models
+{
+    public class ChoreAssignment
+    {
+        public int ID { get; set; }
+        public int ChoreId {get; set; }
+        public int UserId { get; set; }
+        public Enumerators.ChoreStatus  ChoreStatus { get; set; }
+         
+    }
+}
