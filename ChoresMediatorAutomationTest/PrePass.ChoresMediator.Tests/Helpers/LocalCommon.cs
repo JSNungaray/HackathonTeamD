890757b77918ad9d2316c77f630687fab6bf8cf5
Hema Sivakumar <Hema.Sivakumar@prepass.com>
2025-02-26 17:25:51 -0700

    InitialCommit_TestProject

---
+++ ChoresMediatorAutomationTest/PrePass.ChoresMediator.Tests/Helpers/LocalCommon.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
+
+namespace PrePass.ChoresMediator.Tests.Helpers;
+
+[ExcludeFromCodeCoverage]
+internal static class LocalCommon
+{
+    #region " Private Fields "
+
+
+    #endregion
+    
+    #region " internal Fields "
+    
+    internal static IConfiguration TestConfiguration = new ConfigurationManager();
+
+    #endregion
+
+    #region " internal Functions "
+    
+    #endregion
+
+    #region " Private Functions "
+
+    public static void InitializeConfiguration()
+    {
+        TestConfiguration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json", optional: true, reloadOnChange: true)
+            .Build();
+    }
+
+    #endregion
+}
---
+++ ChoresMediatorAutomationTest/PrePass.ChoresMediator.Tests/Tests/HomePageTests.cs
@@ -0,0 +1,56 @@
+﻿// < Summary >
+// This file contains Playwright Automation tests for the ChoresMediator UI Homepage.
+// TERM: TransactionBox => Transaction on the scrolling screen.
+// TERM: SummaryBox => The transaction details that show up as a summary in a new panel.
+// </Summary>
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrePass.ChoresMediator.Tests.Pages;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+using PrePass.ChoresMediator.Tests.Helpers;
+using static System.Net.Mime.MediaTypeNames;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+using System.ComponentModel;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics.Metrics;
+
+namespace PrePass.ChoresMediator.Tests.Tests
+{
+    [TestClass]
+    public class HomePageTests : BaseClass
+    {
+        private HomePage? _homePage;
+        private PageActions? _pageActions;
+
+        #region "Tests"
+
+
+        [TestMethod, TestCategory("Smoke")]
+        public async Task CheckHomePageUrl()
+        {
+            Console.WriteLine("---------- Begin Test: Verify URL is correct ----------");
+            try
+            {
+                _pageActions = new PageActions(Page);
+                Console.WriteLine("Initialize Page...");
+                Assert.IsNotNull(Page.Url,"URL is null");
+                await Page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+                await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                throw;
+            }
+            finally
+            {
+                //Use for test cleanup
+                Console.WriteLine("---------- End Test: Verify URL is correct ----------");
+            }
+        }
+    }
+    #endregion
+}
